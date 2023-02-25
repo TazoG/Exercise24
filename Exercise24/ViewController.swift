508e4114919e9e4e7d16b6e336048cc4c903137a
Tazo <tazogigitashvili@gmail.com>
2023-02-25 22:36:40 +0400

    Initial Commit

---
+++ Exercise24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise24
+//
+//  Created by Tazo Gigitashvili on 25.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
