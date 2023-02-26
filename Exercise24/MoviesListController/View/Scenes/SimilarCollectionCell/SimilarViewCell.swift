d85611df2aebb09f568b5297f3bd953147e73e77
Tazo <tazogigitashvili@gmail.com>
2023-02-26 21:36:10 +0400

    Similar View Cell init

---
+++ Exercise24/MoviesListController/View/Scenes/SimilarCollectionCell/SimilarViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SimilarViewCell.swift
+//  Exercise24
+//
+//  Created by Tazo Gigitashvili on 26.02.23.
+//
+
+import UIKit
+
+class SimilarViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
