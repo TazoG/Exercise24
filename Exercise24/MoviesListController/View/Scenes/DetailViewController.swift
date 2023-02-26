869b489b51c6caddcdfc18d3ddc58cb9b05d476d
Tazo <tazogigitashvili@gmail.com>
2023-02-26 21:29:42 +0400

    Master branch detail vc

---
+++ Exercise24/MoviesListController/View/Scenes/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Exercise24
+//
+//  Created by Tazo Gigitashvili on 26.02.23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
