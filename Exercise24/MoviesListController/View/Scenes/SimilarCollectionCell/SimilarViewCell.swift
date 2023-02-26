455f424ac12b8e1a5f409ba3e5c023a3fa04a10f
Tazo <tazogigitashvili@gmail.com>
2023-02-26 21:38:36 +0400

    similar copied

--- Exercise24/MoviesListController/View/Scenes/SimilarCollectionCell/SimilarViewCell.swift
+++ Exercise24/MoviesListController/View/Scenes/SimilarCollectionCell/SimilarViewCell.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 
-class SimilarViewCell: UICollectionViewCell {
-
+class SimilarMovieCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var favouriteButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        cellImageView.contentMode = .scaleToFill
+        cellImageView.layer.cornerRadius = 20
+    }
+    
+    func configure(with movie: Movie) {
+        if let imageUrl = URL(string: "https://image.tmdb.org/t/p/w500\(movie.image)") {
+            let task = URLSession.shared.dataTask(with: imageUrl) { data, response, error in
+                if let error = error {
+                    print("Error loading movie image: \(error)")
+                    return
+                }
+                
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.cellImageView.image = image
+                    }
+                }
+            }
+            task.resume()
+        }
     }
 
+    
+    
+    @IBAction func favouriteButtonTapped(_ sender: UIButton) {
+        print("Favourited")
+    }
+    
 }
