66cd19feef94cc1aa5e47755b2c8f896c6a9f6be
Tazo <tazogigitashvili@gmail.com>
2023-02-26 12:56:30 +0400

    Created sign up outlets and linked

--- Exercise24/SignUpViewController.swift
+++ Exercise24/SignUpViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func signUpTapped(_ sender: UIButton) {
+        
+    }
+    
+    
 }
 
